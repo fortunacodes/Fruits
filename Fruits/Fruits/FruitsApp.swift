8a5dd99ad70fa8f5787b70b4af1868633fd0a65c
jesusfortuna <fortuna.codes@gmail.com>
2023-05-29 20:55:25 +0200

    Initial set up

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Jesús  Fortuna on 29/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
