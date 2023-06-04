bb6a31822158d1161782ee7b9440e5378431f41b
jesusfortuna <fortuna.codes@gmail.com>
2023-06-04 20:47:20 +0200

    --

---
+++ Fruits/Fruits/View/StartButtonView.swift
@@ -0,0 +1,25 @@
+//
+//  StartButtonView.swift
+//  Fruits
+//
+//  Created by Jesús  Fortuna on 29/5/23.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
