dce06bfc9db43fb801ff2e34ed7fb30cf5bf3ca1
jesusfortuna <fortuna.codes@gmail.com>
2023-06-05 10:43:42 +0200

    Fruits list has been implemented

--- Fruits/Fruits/App/ContentView.swift
+++ Fruits/Fruits/App/ContentView.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("isOnboarding") var isOnboarding: Bool?
-
+    // MARK: - PROPERTY
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: - BODY
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-                .onTapGesture {
-                    isOnboarding = true
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { fruit in
+                    FruitRowView(fruit: fruit)
+                        .padding(.vertical, 4)
                 }
-        }
-        .padding()
+            }.navigationTitle("Fruits")
+        }//: NAVIGATION
     }
 }
 
