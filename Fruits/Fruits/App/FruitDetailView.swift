e0b54cf735cc5f65583843082ce6d37ba0a19dfa
jesusfortuna <fortuna.codes@gmail.com>
2023-06-05 11:12:31 +0200

    FruitDetailView is implemented using NavigationView

---
+++ Fruits/Fruits/App/FruitDetailView.swift
@@ -0,0 +1,70 @@
+//
+//  FruitDetailView.swift
+//  Fruits
+//
+//  Created by Jesús  Fortuna on 5/6/23.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    // MARK: - PROPERTY
+    var fruit: Fruit
+    
+    // MARK: - BODY
+    var body: some View {
+        
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(alignment: .center, spacing: 20){
+                    
+                    //MARK: HEADER
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack (alignment: .leading, spacing: 20){
+                        //MARK: TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                    
+                        //MARK: HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        //MARK: NUTRIENTS
+                        
+                        
+                        //MARK: SUBHEADLINE
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                    
+                        //MARK: DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        //MARK: LINK
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                        
+                    }//: VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }//: VSTACK
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            }//: SCROLL
+            .edgesIgnoringSafeArea(.top)
+        }//: NAVIATION
+    }
+}
+
+// MARK: - PREVIEW
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[2])
+    }
+}
